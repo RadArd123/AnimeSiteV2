04a1709950b9ee607d1b8923eb84f12294d99cde
RadArd123 <radu.ardelean15.92@gmail.com>
2025-04-28 19:41:03 +0300

    last update

--- backend/index.js
+++ backend/index.js
@@ -19,7 +19,7 @@ app.use(cookieParser());
 
 
 app.use(cors({
-    origin: ["http://localhost:5173", "https://anime-site-v2-seven.vercel.app"],
+    origin: ["http://localhost:5173", "https://animesitev2-frontend.onrender.com/"],
     credentials: true
 }));
 
