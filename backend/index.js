29d63e79482bd33ab3c98e6fb10a6b2e8cc3b99c
RadArd123 <radu.ardelean15.92@gmail.com>
2025-04-28 14:32:29 +0300

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -16,7 +16,11 @@ const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({origin: "http://localhost:5173", credentials: true}));
+app.use(cors({
+    origin: ["http://localhost:5173", "https://animesitev2-frontend.onrender.com"], 
+    credentials: true
+  }));
+
 
 app.use("/api/auth", authRoutes);
 app.use("/api/anime", animeRoutes);
